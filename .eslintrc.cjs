0e02fa9f7b67d3f55d1967fd2892a6fd339d5349
yelee park <dev.yelee@gmail.com>
2023-11-06 21:37:56 +0900

    chore: eslint airbnb 적용

--- .eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'next/core-web-vitals'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-};
