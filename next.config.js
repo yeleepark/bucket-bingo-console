903b155e729eb4a7c3c8b00836ff62c8d2015c28
yelee park <dev.yelee@gmail.com>
2023-11-17 18:47:17 +0900

    chore: next/bundle-analyzer 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = {
+  experimental: { webpackBuildWorker: true },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
