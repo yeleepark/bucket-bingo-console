42dbd3f40c1ca2692bcf0249f4a73dd163b756bd
yelee park <dev.yelee@gmail.com>
2023-11-19 20:52:13 +0900

    chore: App router -> Pages router 변경

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable object-curly-newline */
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
