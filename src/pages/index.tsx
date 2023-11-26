94a11059f39ad423112b58427590fe16fb8dfda1
yelee park <dev.yelee@gmail.com>
2023-11-26 15:29:25 +0900

    feat: main 페이지 기본 레이아웃 구성 - theme color 추가 (blueGrey) - AppBar, Main, Footer 구성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,60 @@
+import {
+  AppBar,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Container,
+  Grid,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
 export default function Home() {
-  return <div>C o m i n g S o o n</div>;
+  return (
+    <>
+      <AppBar position="relative">
+        <Toolbar>
+          <Container maxWidth="xl">AppBar</Container>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Box sx={{ bgcolor: 'background.paper' }}>
+          <Box sx={{ bgcolor: `blueGrey.50`, py: 10 }}>
+            <Container maxWidth="xl">Intro</Container>
+          </Box>
+          <Box sx={{ py: 10 }}>
+            <Container maxWidth="xl">
+              <Typography variant="h3">Title</Typography>
+              <Grid
+                container
+                columns={{ xs: 1, sm: 2, md: 3, lg: 4 }}
+                spacing={4}
+              >
+                {Array.from({ length: 10 }).map((_, index) => (
+                  <Grid key={index} item xs={1}>
+                    <Card>
+                      <CardHeader title={`title`} subheader={`info`} />
+                      <CardMedia>
+                        <CardContent>컨텐츠</CardContent>
+                      </CardMedia>
+                      <CardActions disableSpacing>카드액션</CardActions>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
+          </Box>
+        </Box>
+      </main>
+      <Box
+        component="footer"
+        sx={{ bgcolor: `grey.800`, color: `common.white`, py: 10 }}
+      >
+        <Container maxWidth="xl">footer</Container>
+      </Box>
+    </>
+  );
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,5 +1,6 @@
 import { Noto_Sans_KR } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
+import { blueGrey } from '@mui/material/colors';
 
 export const noto_sans = Noto_Sans_KR({
   weight: ['100', '300', '400', '500', '700'],
@@ -8,8 +9,19 @@ export const noto_sans = Noto_Sans_KR({
   fallback: ['system-ui'],
 });
 
+declare module '@mui/material/styles' {
+  interface Palette {
+    blueGrey: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    blueGrey?: PaletteOptions['primary'];
+  }
+}
+
 // Create a theme instance.
 const theme = createTheme({
+  palette: { blueGrey: { ...blueGrey } },
   typography: {
     fontFamily: noto_sans.style.fontFamily,
   },
