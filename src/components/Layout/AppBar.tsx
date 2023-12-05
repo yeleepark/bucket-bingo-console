a3c5b49c017117c9fa0b55dfb275851fbd8b8d52
yelee park <dev.yelee@gmail.com>
2023-12-05 18:47:09 +0900

    feat: AppBar에 추가버튼 생성

--- src/components/Layout/AppBar.tsx
+++ src/components/Layout/AppBar.tsx
@@ -1,9 +1,24 @@
-import { AppBar as MuiAppBar, Container, Toolbar } from '@mui/material';
+import {
+  AppBar as MuiAppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+} from '@mui/material';
+import GridViewSharpIcon from '@mui/icons-material/GridViewSharp';
 
 const AppBar = () => (
-  <MuiAppBar position="relative">
-    <Toolbar>
-      <Container maxWidth="xl">AppBar</Container>
+  <MuiAppBar position="relative" color="transparent">
+    <Toolbar sx={{ justifyContent: `space-between` }}>
+      <Box display="flex" alignItems="center">
+        <GridViewSharpIcon color="primary" />
+        <Typography variant="h6" noWrap ml={1}>
+          BUCKET BINGO
+        </Typography>
+      </Box>
+      <Button variant="contained" color="primary">
+        만들기
+      </Button>
     </Toolbar>
   </MuiAppBar>
 );
