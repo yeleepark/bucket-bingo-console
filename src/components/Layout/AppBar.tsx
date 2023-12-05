78734788c0325c413fe65ce69cd3d34a74a7092b
yelee park <dev.yelee@gmail.com>
2023-12-05 19:12:19 +0900

    feat: 빙고 생성 팝업 레이아웃 구성

--- src/components/Layout/AppBar.tsx
+++ src/components/Layout/AppBar.tsx
@@ -6,21 +6,34 @@ import {
   Box,
 } from '@mui/material';
 import GridViewSharpIcon from '@mui/icons-material/GridViewSharp';
+import { useState } from 'react';
+import CreateBingoBoard from '@features/CreateBingoBoard';
 
-const AppBar = () => (
-  <MuiAppBar position="relative" color="transparent">
-    <Toolbar sx={{ justifyContent: `space-between` }}>
-      <Box display="flex" alignItems="center">
-        <GridViewSharpIcon color="primary" />
-        <Typography variant="h6" noWrap ml={1}>
-          BUCKET BINGO
-        </Typography>
-      </Box>
-      <Button variant="contained" color="primary">
-        만들기
-      </Button>
-    </Toolbar>
-  </MuiAppBar>
-);
+const AppBar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <MuiAppBar position="relative" color="transparent">
+        <Toolbar sx={{ justifyContent: `space-between` }}>
+          <Box display="flex" alignItems="center">
+            <GridViewSharpIcon color="primary" />
+            <Typography variant="h6" noWrap ml={1}>
+              BUCKET BINGO
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setOpen(true)}
+          >
+            만들기
+          </Button>
+        </Toolbar>
+      </MuiAppBar>
+      <CreateBingoBoard open={open} onClose={() => setOpen(false)} />
+    </>
+  );
+};
 
 export default AppBar;
