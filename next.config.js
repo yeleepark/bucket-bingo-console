f3e24e1c9081b463d0b2e13caaf61d05c012f8ea
yelee park <dev.yelee@gmail.com>
2023-12-26 21:04:25 +0900

    chore: next.config.js에 compiler 설정

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const nextConfig = {
+  compiler: { styledComponents: true },
   experimental: { webpackBuildWorker: true },
 };
 
